d6cec07f4c6b0ecc89c439820bbb9825d1f70896
Andrew Zhang <zandrew404@gmail.com>
2026-03-29 17:54:33 -0700

    gitsigns

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -74,6 +74,47 @@ require("lazy").setup({
     end,
   },
   {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "^" },
+        changedelete = { text = "~" },
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]h", bang = true })
+          else
+            gs.nav_hunk("next")
+          end
+        end, "Next hunk")
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[h", bang = true })
+          else
+            gs.nav_hunk("prev")
+          end
+        end, "Previous hunk")
+        map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+        map("n", "<leader>hb", function()
+          gs.blame_line({ full = true })
+        end, "Blame line")
+      end,
+    },
+  },
+  {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     event = { "BufReadPre", "BufNewFile" },
